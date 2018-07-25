4d34cb6e6f617755cc60bfa452167fd535c19521
tangsongmingqing <chenyany@hu-berlin.de>
2018-07-25 09:38:51 +0100

    tidy up raw data code

--- code/import data.R
+++ code/import data.R
@@ -55,3 +55,25 @@ s1 = beijing2018[ ,-c(27:33)]
 s2 = s1[which(s1$type=='PM2.5'), ]
 colnames(s2) = c("date", "hour", "type", 1:28)
 saveRDS(s2, file = "~/AirQualityBeijing/data/PM2.5/pm2.5_2018.RDS")
+
+#####################PM2.5 concentration from U.S. embassy#############
+# clear variables and close windows
+rm(list = ls(all = TRUE))
+graphics.off()
+setwd("~/AirQualityBeijing/raw data/USA_PM2.5/tidy US PM2.5")
+us2013 = read_csv("beijing2013.csv")
+s1 = us2013[ ,-c(1,2,8:10)] 
+saveRDS(s1, file = "~/AirQualityBeijing/data/usPM2.5/usPM2.52013.RDS")
+us2014 = read_csv("beijing2014.csv")
+s2 = us2014[ ,-c(1,2,8:10)] 
+saveRDS(s2, file = "~/AirQualityBeijing/data/usPM2.5/usPM2.52014.RDS")
+us2015 = read_csv("beijing2015.csv")
+s3 = us2015[ ,-c(1,2,8:10)] 
+saveRDS(s3, file = "~/AirQualityBeijing/data/usPM2.5/usPM2.52015.RDS")
+us2016 = read_csv("beijing2016.csv")
+s4 = us2016[ ,-c(1,2,8:10)] 
+saveRDS(s4, file = "~/AirQualityBeijing/data/usPM2.5/usPM2.52016.RDS")
+us2017 = read_csv("beijing2017.csv")
+s5 = us2017[ ,-c(1,2,8:10)] 
+saveRDS(s5, file = "~/AirQualityBeijing/data/usPM2.5/usPM2.52017.RDS")
+
